b271ce6642dd792bfae74f2fdb467503f0a6a5c2
ajimine19 <ajimine19@up.edu>
2018-10-20 21:17:25 -0700

    my branch

--- src/constants/routes.js
+++ src/constants/routes.js
@@ -1,7 +1,6 @@
-export const LANDING = "/";
 export const HOME = "/home";
 export const CODING = "/coding";
 export const PHONE = "/phone";
 export const USER_DASH = "/user-dash";
 export const RECRUIT_DASH = "/recruit-dash";
-export const FORM = "/form";
+export const FORM = "/";
