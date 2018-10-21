dff4f7c1f6c65ab51e9547f818a0d8670e08c176
ajimine19 <ajimine19@up.edu>
2018-10-20 21:49:49 -0700

    sorta

--- src/components/form.js
+++ src/components/form.js
@@ -1,2 +1,36 @@
 import React, { Component } from "react";
-import { Button, Form } from "react-bootstrap";
+import { Button, Form, Grid, Row, Col } from "react-bootstrap";
+
+class myForm extends Component {
+  render() {
+    return (
+      <div className="container">
+        <div className="row">
+
+          <div className="col">
+            One of three columns
+          </div>
+
+          <div className="col">
+            One of three columns
+          </div>
+
+          <div className="col">
+            One of three columns
+          </div>
+
+        </div>
+          <h1>FORMS</h1>
+          <form>
+          <label>
+              Name:
+              <input type="text" name="name" />
+            </label>
+            <input type="submit" value="Submit" />
+          </form>
+      </div>
+    );
+  }
+}
+
+export default myForm;
