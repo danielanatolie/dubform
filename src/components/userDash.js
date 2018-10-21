3471c737f05467999991eabbc89375c8962a977a
lockheedmartian <lockheedmartian@gmail.com>
2018-10-21 04:10:49 -0700

    update stuff

--- src/components/userDash.js
+++ src/components/userDash.js
@@ -1,118 +1,115 @@
 import React, { Component } from "react";
 import { Link, withRouter } from "react-router-dom";
 import * as routes from "../constants/routes";
-import './userDash.css';
+import "./userDash.css";
+import "./companyEvent.png";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faInfoCircle } from "@fortawesome/free-solid-svg-icons";
+import { Modal, Button, Image } from "react-bootstrap";
 
 var hello = 1;
 
 class UserDash extends Component {
-
     constructor(props) {
         super(props);
         this.state = {
-            completeBars: hello
+            completeBars: hello,
+            show: false
         };
+        this.handleHide = this.handleHide.bind(this);
     }
 
     updateBars() {
         hello++;
     }
 
-    render() {
-        return (
-            <div>
-                <Link to={routes.FORM}> 
-                    <div className="home-link">
-                        Dubform
-                    </div>
-                </Link>
-                <div className="user-name"> 
-                    John Doe
-                </div>
-                <div className="dashHeader">
-                    Your Companies:
-                </div>
-                <div className="dashContainer">
-                    <div className="company">
-                        Facebook   
-                        {this.state.completeBars === 1 ? (
-                            <div className="progress">
-                                <div className="progComplete progSection">
-                                    <div className="progButton">
-                                        <Link to={routes.CODING} onClick={() => this.updateBars()}>
-                                            Start Coding Challenge
-                                        </Link>
-                                    </div>
-                                </div>
-                                <div id="test" className="progSection">
-                                </div>
-                                <div className="progSection">
-                                </div>
-                            </div>
-                        ) : this.state.completeBars === 2 ? (
-                            <div className="progress">
-                                <div className="progComplete progSection">
-                                </div>
-                                <div className="progComplete progSection">
-                                    <div className="progButton">
-                                        <Link to={routes.PHONE} onClick={() => this.updateBars()}>
-                                            Schedule Phone Interview
-                                        </Link>
-                                    </div>
-                                </div>
-                                <div className="progSection">
-                                </div>
-                            </div>
-                        ) : (
-                            <div className="progress">
-                                <div className="progComplete progSection">
-                                </div>
-                                <div className="progComplete progSection">
-                                </div>
-                                <div className="progComplete progSection">
-                                    <div className="progButton">
-                                        Pending Offer
-                                    </div>
-                                </div>
-                            </div>
-                        )}
-                    </div>
-                    <div className="spacer"></div>
-                    <div className="company">
-                        Google
-                        <div className="progress">
-                            <div className="progComplete progSection">
-                            </div>
-                            <div className="progComplete progSection">
-                                <div className="progButton">
-                                    Schedule Phone Interview
-                                </div>
-                            </div>
-                            <div className="progSection">
-                            </div>
+  handleHide() {
+    this.setState({ show: false });
+  }
+
+  showDropdown() {
+    this.setState({ completeBars: 2 });
+    console.log(this.state.completeBars);
+    console.log("hi");
+  }
+  render() {
+    const companyEvent =
+      "Recruiting Event: \n 8:30PM Date: Sun. Oct. 21, 2018. Where: University of Washington, info.science building Registration Details: {URL_LINK} Interested? [YES][NO]";
+    return (
+      <div>
+        <Link to={routes.FORM}>
+          <div className="home-link">Dubform</div>
+        </Link>
+        <div className="user-name">John Doe</div>
+        <div className="dashHeader">Your Companies:</div>
+        <div className="dashContainer">
+          <div className="company">
+            Facebook{" "}
+            <FontAwesomeIcon
+              icon={faInfoCircle}
+              size="xs"
+              onClick={() => this.setState({ show: true })}
+            />
+            <Modal
+              show={this.state.show}
+              onHide={this.handleHide}
+              container={this}
+              aria-labelledby="contained-modal-title"
+            >
+              <Modal.Header closeButton>
+                <Modal.Title id="contained-modal-title">
+                  Upcoming Company Events LALALLALALAL
+                </Modal.Title>
+              </Modal.Header>
+              <Modal.Body>
+                <div className="display-linebreak">{companyEvent}</div>{" "}
+                <Image src="./companyEvent.png" rounded />
+              </Modal.Body>
+              <Modal.Footer>
+                <Button onClick={this.handleHide}>Close</Button>
+              </Modal.Footer>
+            </Modal>
+            {this.state.completeBars === 1 ? (
+                <div className="progress">
+                    <div className="progComplete progSection">
+                        <div className="progButton">
+                            <Link to={routes.CODING} onClick={() => this.updateBars()}>
+                                Start Coding Challenge
+                            </Link>
                         </div>
                     </div>
-                    <div className="spacer"></div>
-                    <div className="company">
-                        Amazon
-                        <div className="progress">
-                            <div className="progCancelled progSection">
-                                <div className="progDull progButton">
-                                   Failed Coding Challenge :(
-                                </div>
-                            </div>
-                            <div className=" progSection">
-                                
-                            </div>
-                            <div className="progSection">
-                            </div>
+                    <div id="test" className="progSection">
+                    </div>
+                    <div className="progSection">
+                    </div>
+                </div>
+            ) : this.state.completeBars === 2 ? (
+                <div className="progress">
+                    <div className="progComplete progSection">
+                    </div>
+                    <div className="progComplete progSection">
+                        <div className="progButton">
+                            <Link to={routes.PHONE} onClick={() => this.updateBars()}>
+                                Schedule Phone Interview
+                            </Link>
                         </div>
                     </div>
-                //hi
+                    <div className="progSection">
+                    </div>
                 </div>
-                <div className="progSection" />
-              </div>
-            )}
+            ) : (
+                <div className="progress">
+                    <div className="progComplete progSection">
+                    </div>
+                    <div className="progComplete progSection">
+                    </div>
+                    <div className="progComplete progSection">
+                        <div className="progButton">
+                            Pending Offer
+                        </div>
+                    </div>
+                </div>
+                )}
           </div>
           <div className="spacer" />
           <div className="company">
@@ -142,7 +139,42 @@ class UserDash extends Component {
             <div className="progress">
               <div className="progComplete progSection" />
               <div className="progComplete progSection">
-                <div className="progButton">Schedule Phone Interview</div>
+                <div className="progButton">Do Phone Interview</div>
+              </div>
+              <div className="progSection" />
+            </div>
+          </div>
+          <div className="spacer" />
+          <div className="company">
+            Amazon{" "}
+            <FontAwesomeIcon
+              icon={faInfoCircle}
+              size="xs"
+              onClick={() => this.setState({ show: true })}
+            />
+            <Modal
+              show={this.state.show}
+              onHide={this.handleHide}
+              container={this}
+              aria-labelledby="contained-modal-title"
+            >
+              <Modal.Header closeButton>
+                <Modal.Title id="contained-modal-title">
+                  Upcoming Company Events
+                </Modal.Title>
+              </Modal.Header>
+              <Image src="http://15f.dubhacks.co/images/fb_link.png" rounded />
+              <Modal.Body>{companyEvent}</Modal.Body>
+              <Modal.Footer>
+                <Button onClick={this.handleHide}>Close</Button>
+              </Modal.Footer>
+            </Modal>
+            <div className="progress">
+              <div className="progCancelled progSection">
+                <div className="progDull progButton">Failed Coding Challenge :(</div>
+              </div>
+              <div className="progSection">
+                
               </div>
               <div className="progSection" />
             </div>
