8751c0cb15a48c379309898db6019e4dec42dcd0
lockheedmartian <lockheedmartian@gmail.com>
2018-10-20 20:23:56 -0700

    update files

--- src/components/app.js
+++ src/components/app.js
@@ -1,7 +1,6 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
-import { BrowserRouter as Router, Route } from "../react-router-dom";
-import './App.css';
+import { BrowserRouter as Router, Route } from "react-router-dom";
+import './app.css';
 import Coding from './coding';
 import Form from './form';
 import Phone from './phone';
@@ -15,7 +14,6 @@ const App = () => (
       <Route exact path={routes.FORM} component={() => <Form />} />
       <Route exact path={routes.CODING} component={() => <Coding />} />
       <Route exact path={routes.PHONE} component={() => <Phone />} />
-      <Route exact path={routes.HOME} component={() => <HomePage />} />
       <Route exact path={routes.USER_DASH} component={() => <UserDash />} />
       <Route exact path={routes.RECRUIT_DASH} render={() => <RecruitDash />} />
     </div>
---
+++ src/constants/routes.js
@@ -0,0 +1,7 @@
+export const LANDING = "/";
+export const HOME = "/home";
+export const CODING = "/coding";
+export const PHONE = "/phone";
+export const USER_DASH = "/user-dash";
+export const RECRUIT_DASH = "/recruit-dash";
+export const FORM = "/form";
