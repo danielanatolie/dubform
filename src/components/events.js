7bbbf53182741dcf46ed3a7964fa50fbb3173e6d
Daniel Anatolie <daniel.anatolie@gmail.com>
2018-10-21 09:42:01 -0700

    Updated Events page

--- src/components/events.js
+++ src/components/events.js
@@ -1,6 +1,6 @@
 import React, { Component } from "react";
 import {Link, withRouter} from "react-router-dom";
-import { Grid, Col, Row, Modal, Button, Image, Navbar, Nav, NavItem, MenuItem, NavDropdown, FormGroup, Carousel, Badge } from "react-bootstrap";
+import { Grid, Col, Row, Modal, Button, Image, Navbar, Nav, NavItem, MenuItem, NavDropdown, FormGroup, Carousel, Badge, Thumbnail } from "react-bootstrap";
 import * as routes from "../constants/routes";
 import "./seattle.png";
 
@@ -32,10 +32,30 @@ class myEvents extends Component {
       </Navbar.Collapse>
 
       </Navbar>
+      <Carousel>
+        <Carousel.Item>
+          <img width={1400} height={300}  src="https://www.marsdd.com/wp-content/uploads/2018/06/job-fairs-hackathons-1998x1100.jpg" />
+          <Carousel.Caption>
+            <h1>Hackathons</h1>
+          </Carousel.Caption>
+        </Carousel.Item>
+        <Carousel.Item>
+          <img width={1400} height={300} alt="500x500" src="https://www.greedge.com/blog/wp-content/uploads/2017/08/1429319jakarta-career-day.jpg" />
+          <Carousel.Caption>
+            <h1>Tech Events</h1>
+          </Carousel.Caption>
+        </Carousel.Item>
+        <Carousel.Item>
+          <img width={1400} height={300} alt="500x500" src="https://images.ctfassets.net/7ca9kifzimh6/4E4BoAIvHq2KIIuMQakEq2/089f44baebb3e06399894bcbc458efcd/technology.jpg?q=50&fl=progressive" />
+          <Carousel.Caption>
+            <h1>Internships</h1>
+          </Carousel.Caption>
+        </Carousel.Item>
+      </Carousel>
       <Grid>
           <Row className="show-grid">
             <Col md={4}>
-              <Image src="./seattle.png" rounded />
+              <Thumbnail alt="171x180" src="https://pbs.twimg.com/profile_images/1012542719661469696/0ReXCVXV_400x400.jpg" rounded />
                 <h4>Tech Talk - Seattle</h4>
                 <p>
                   Time: 7:00PM
@@ -53,7 +73,7 @@ class myEvents extends Component {
                 </p>
             </Col>
             <Col md={4}>
-              <Image src="/companyEvent.png" rounded />
+              <Thumbnail alt="171x180" src="https://teenlife.s3.amazonaws.com/media/uploads/listings/BLqgL7cNrgto.jpg" rounded />
               <h4>UBC Career Days</h4>
               <p>
                 Time: 5:00PM - 7:00PM
@@ -70,7 +90,7 @@ class myEvents extends Component {
               </p>
             </Col>
             <Col md={4}>
-              <Image src="/seattle.png" rounded />
+              <Thumbnail alt="171x180" src="http://weclipart.com/gimg/330CFA72211A0B7C/UW_Logo_University_of_Washington.jpg" rounded />
               <h4>Meetup Group</h4>
               <p>
                 Time: 6:00 - 7:30PM
